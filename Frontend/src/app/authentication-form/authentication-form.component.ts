6990f904d01d49a113134cb82814c58b29cde6a3
Dmytro <web11rider@gmail.com>
2025-07-01 17:07:42 +0300

    Frontend: html and css for sign up form

--- Frontend/src/app/authentication-form/authentication-form.component.ts
+++ Frontend/src/app/authentication-form/authentication-form.component.ts
@@ -1,15 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { AbstractControl, FormBuilder, ReactiveFormsModule, ValidationErrors, ValidatorFn, Validators } from '@angular/forms';
 import { ClickStopPropagationDirective } from '../click-stop-propagation.directive';
 
 @Component({
   selector: 'app-authentication-form',
-  imports: [ClickStopPropagationDirective],
+  imports: [ClickStopPropagationDirective, ReactiveFormsModule],
   templateUrl: './authentication-form.component.html',
   styleUrl: './authentication-form.component.scss'
 })
 export class AuthenticationFormComponent {
 
+  private formBuilder = inject(FormBuilder);
+
   isAuthFormShown: Boolean = true;
+
+  authForm = this.formBuilder.group({
+    username: ['', Validators.required],
+    email: ['', Validators.compose([Validators.required, Validators.email])],
+    password: ['', Validators.required],
+    confirmPassword: ['', Validators.required],
+  },
+  {
+    validators: this.confirmPasswordValidator('password', 'confirmPassword')
+  });
 
   authFormToggle() {
     const bodyElement = document.body;
@@ -23,4 +36,27 @@ export class AuthenticationFormComponent {
     }
   }
 
+  onSubmit() {
+  }
+
+  confirmPasswordValidator(passwordControl: string, confirmPasswordControlName: string): ValidatorFn {
+    return (control: AbstractControl): ValidationErrors | null => {
+      const password = control.get(passwordControl);
+      const confirmPassword = control.get(confirmPasswordControlName);
+
+      if(confirmPassword?.errors && !confirmPassword!.errors?.['confirmedPasswordValidator']) {
+        return null;
+      }
+
+      if(password?.value !== confirmPassword?.value) {
+        const error = { confirmedPasswordValidator: "Passwords do not match"};
+        confirmPassword?.setErrors(error);
+        return error;
+      }
+      else {
+        confirmPassword?.setErrors(null);
+        return null;
+      }
+    }
+  }
 }
---
+++ Frontend/src/app/confirm-password-validator.directive.ts
@@ -0,0 +1,10 @@
+import { Directive } from '@angular/core';
+
+@Directive({
+  selector: '[appConfirmPasswordValidator]'
+})
+export class ConfirmPasswordValidatorDirective {
+
+  constructor() { }
+
+}
